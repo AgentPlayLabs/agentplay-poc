e45ba572aec2f8a1b46de427b8ea8c82bdea88c4
lizhi <12884791+liiil825@user.noreply.gitee.com>
2025-02-08 14:14:14 +0800

    fix: resolve React key and ref issues
    - Add unique keys to sidebar skeleton items
    - Remove unnecessary forwardRef in ChatMessageList component

--- client/src/components/ui/chat/chat-message-list.tsx
+++ client/src/components/ui/chat/chat-message-list.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { ArrowDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-interface ChatMessageListProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface ChatMessageListProps extends React.HTMLAttributes<HTMLDivElement> {
     scrollRef: React.RefObject<HTMLDivElement | null>;
     isAtBottom: boolean;
     scrollToBottom: () => void;
@@ -10,37 +10,39 @@ interface ChatMessageListProps extends React.HTMLAttributes<HTMLDivElement> {
     smooth?: boolean;
 }
 
-const ChatMessageList = React.forwardRef<HTMLDivElement, ChatMessageListProps>(
-    ({ className, children, scrollRef, isAtBottom, scrollToBottom, disableAutoScroll, ...props }) => {
-        return (
-            <div className="relative w-full h-full">
-                <div
-                    className={`flex flex-col w-full h-full p-4 overflow-y-auto ${className}`}
-                    ref={scrollRef}
-                    onWheel={disableAutoScroll}
-                    onTouchMove={disableAutoScroll}
-                    {...props}
-                >
-                    <div className="flex flex-col gap-6">{children}</div>
-                </div>
-
-                {!isAtBottom && (
-                    <Button
-                        onClick={() => {
-                            scrollToBottom();
-                        }}
-                        size="icon"
-                        variant="outline"
-                        className="absolute bottom-2 left-1/2 transform -translate-x-1/2 inline-flex rounded-full shadow-md"
-                    >
-                        <ArrowDown className="h-4 w-4" />
-                    </Button>
-                )}
+export function ChatMessageList({ 
+    className, 
+    children, 
+    scrollRef, 
+    isAtBottom, 
+    scrollToBottom, 
+    disableAutoScroll, 
+    ...props 
+}: ChatMessageListProps) {
+    return (
+        <div className="relative w-full h-full">
+            <div
+                className={`flex flex-col w-full h-full p-4 overflow-y-auto ${className}`}
+                ref={scrollRef}
+                onWheel={disableAutoScroll}
+                onTouchMove={disableAutoScroll}
+                {...props}
+            >
+                <div className="flex flex-col gap-6">{children}</div>
             </div>
-        );
-    }
-);
 
-ChatMessageList.displayName = "ChatMessageList";
-
-export { ChatMessageList };
+            {!isAtBottom && (
+                <Button
+                    onClick={() => {
+                        scrollToBottom();
+                    }}
+                    size="icon"
+                    variant="outline"
+                    className="absolute bottom-2 left-1/2 transform -translate-x-1/2 inline-flex rounded-full shadow-md"
+                >
+                    <ArrowDown className="h-4 w-4" />
+                </Button>
+            )}
+        </div>
+    );
+}
